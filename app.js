121b6d5e273555d0b5f5ab1de5d60b6c9419362b
Bruno Rocha <bruezr@icloud.com>
2021-03-20 18:53:17 -0300

    habilito CORS

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ dbConnection();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/api/feed", feedRoutes);
 
 app.use("/", (req, res, next) => {
