c4b0b037842aa49cb87c1439ad436f2b562554d5
Bruno Rocha <bruezr@icloud.com>
2021-03-23 16:03:32 -0300

    agrego model de user

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "./.env" });
+
+const jwtPrivateKey = process.env.JWT_PRIVATE_KEY;
+
+const userShema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+    minlenght: 5,
+  },
+  email: {
+    type: String,
+    required: true,
+    maxlength: 255,
+    minlenght: 5,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    maxlength: 1024,
+    minlenght: 5,
+  },
+});
+
+userShema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, jwtPrivateKey);
+  return token;
+};
+
+module.exports = mongoose.model("User", userShema);
